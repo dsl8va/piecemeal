36654468f21cd7f36b382dca2ba85b4283abb864
Daniel Lee <dsl8va@gmail.com>
2020-12-19 19:30:27 -0800

    added dynamically rendered home page with featured recipes

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+export default function My404() {
+  return (
+    <Container>
+      <div>Oh no! Looks like this page doesn't exist</div>
+    </Container>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,56 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, CardDeck, Jumbotron } from 'react-bootstrap'
+import CardTemplate from '../components/Card'
 
-export default function Home() {
+export async function getStaticProps(context) {
+  const res = await fetch(`https://api.spoonacular.com/recipes/random?number=3&apiKey=06fd4d74136f48c083cef13657f3a34a`);
+  const data = await res.json();
+  const recipes = data.recipes;
+
+  const title1 = recipes[0].title;
+  const summary1 = recipes[0].summary;
+  const image1 = recipes[0].image;
+  const title2 = recipes[1].title;
+  const summary2 = recipes[1].summary;
+  const image2 = recipes[1].image;
+  const title3 = recipes[2].title;
+  const summary3 = recipes[2].summary;
+  const image3 = recipes[2].image;
+
+  return {
+    props: {
+      title1, summary1, image1, title2, summary2, image2, title3, summary3, image3
+    },
+    revalidate: 86400,
+  }
+}
+
+
+export default function Home({title1, summary1, image1, title2, summary2, image2, title3, summary3, image3}) {
   return (
     <Container>
       <Head>
         <title>piecemeal</title>
       </Head>
-      <Link href='/recipes/random'>
-      <a>Get a random recipe</a>
-      </Link>
-      <div>Welcome to piecemeal. Our mission is to empower you to create delicious meals according to your tastes, diets, or even your food intolerances. Take a look at what we can offer you!</div>
+
+      <Jumbotron fluid>
+        <Container>
+          <h1>Welcome!</h1>
+          <p>
+            Our mission is to empower you to create delicious meals according to your tastes, diets, or even your food intolerances. Take a look at what we can offer you!
+          </p>
+        </Container>
+      </Jumbotron>
+      <h3>Featured recipes:</h3>
+      <CardDeck>
+        <CardTemplate title={title1} description={summary1} image={image1}/>
+        <CardTemplate title={title2} description={summary2} image={image2}/>
+        <CardTemplate title={title3} description={summary3} image={image3}/>
+
+      </CardDeck>
+
     </Container>
   )
 }
