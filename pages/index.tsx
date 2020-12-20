dcf40d94f0eafa42d509b6c8de2d63d0f5c81cdc
Daniel Lee <dsl8va@gmail.com>
2020-12-19 20:03:46 -0800

    working home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,38 +6,35 @@ import React from 'react'
 import { Container, CardDeck, Jumbotron } from 'react-bootstrap'
 import CardTemplate from '../components/Card'
 
-export async function getStaticProps(context) {
-  const res = await fetch(`https://api.spoonacular.com/recipes/random?number=3&apiKey=06fd4d74136f48c083cef13657f3a34a`);
-  const data = await res.json();
-  const recipes = data.recipes;
+// export async function getStaticProps(context) {
+//   const res = await fetch(`https://api.spoonacular.com/recipes/random?number=3&apiKey=06fd4d74136f48c083cef13657f3a34a`);
+//   const data = await res.json();
+//   const recipes = data.recipes;
 
-  const title1 = recipes[0].title;
-  const summary1 = recipes[0].summary;
-  const image1 = recipes[0].image;
-  const title2 = recipes[1].title;
-  const summary2 = recipes[1].summary;
-  const image2 = recipes[1].image;
-  const title3 = recipes[2].title;
-  const summary3 = recipes[2].summary;
-  const image3 = recipes[2].image;
+//   const title1 = recipes[0].title;
+//   const title2 = recipes[1].title;
+//   const title3 = recipes[2].title;
+//   const image1 = recipes[0].image;
+//   const image2 = recipes[1].image;
+//   const image3 = recipes[2].image;
 
-  return {
-    props: {
-      title1, summary1, image1, title2, summary2, image2, title3, summary3, image3
-    },
-    revalidate: 86400,
-  }
-}
+//   return {
+//     props: {
+//       title1, image1, title2, image2, title3, image3
+//     },
+//     revalidate: 86400,
+//   }
+// }
 
 
-export default function Home({title1, summary1, image1, title2, summary2, image2, title3, summary3, image3}) {
+export default function Home({title1, image1, title2, image2, title3, image3}) {
   return (
     <Container>
       <Head>
         <title>piecemeal</title>
       </Head>
 
-      <Jumbotron fluid>
+      <Jumbotron>
         <Container>
           <h1>Welcome!</h1>
           <p>
@@ -47,9 +44,9 @@ export default function Home({title1, summary1, image1, title2, summary2, image2
       </Jumbotron>
       <h3>Featured recipes:</h3>
       <CardDeck>
-        <CardTemplate title={title1} description={summary1} image={image1}/>
-        <CardTemplate title={title2} description={summary2} image={image2}/>
-        <CardTemplate title={title3} description={summary3} image={image3}/>
+        <CardTemplate title={title1} image={image1}/>
+        <CardTemplate title={title2} image={image2}/>
+        <CardTemplate title={title3} image={image3}/>
 
       </CardDeck>
 
